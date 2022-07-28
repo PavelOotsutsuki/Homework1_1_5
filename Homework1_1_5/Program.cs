957925946e817c95a5e4e424670ba75245d66c4e
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-28 16:31:43 +0300

    Добавьте файлы проекта.

---
+++ Homework1_1_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework1_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="Иванов"; 
+            string secondName="Иван"; 
+            string temporarilyVariable; 
+
+            Console.WriteLine("Значение переменной name до перестановки: " + name);
+            Console.WriteLine("Значение переменной secondName до перестановки: " + secondName);
+            temporarilyVariable = name;
+            name = secondName;
+            secondName = temporarilyVariable;
+            Console.WriteLine("Значение переменной name после перестановки: " + name);
+            Console.WriteLine("Значение переменной secondName после перестановки: " + secondName);
+        }
+    }
+}
